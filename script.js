5312963b5774ea321cd8075519686205274b86d2
ProjectIoT17 <projecttiot17@gmail.com>
2026-01-15 01:22:39 +0700

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 window.addEventListener("load", () => {
 
   const mqttStatus = document.getElementById("mqttStatus");
+  const soil = document.getElementById("soil");
+  const volt = document.getElementById("volt");
+  const current = document.getElementById("current");
+  const power = document.getElementById("power");
+  const mode = document.getElementById("mode");
+  const pump = document.getElementById("pump");
+  const threshold = document.getElementById("threshold");
 
   const client = mqtt.connect(
     "wss://446ae662e1794c52a764bff380d64cfe.s1.eu.hivemq.cloud:8884",
@@ -13,7 +20,6 @@ window.addEventListener("load", () => {
   );
 
   client.on("connect", () => {
-    console.log("WEB MQTT CONNECTED");
     mqttStatus.textContent = "CONNECTED";
     mqttStatus.className = "online";
     client.subscribe("irrigation/#");
@@ -29,13 +35,8 @@ window.addEventListener("load", () => {
     mqttStatus.className = "offline";
   });
 
-  client.on("error", err => {
-    console.error("MQTT ERROR:", err);
-  });
-
   client.on("message", (topic, message) => {
     const data = message.toString();
-    console.log(topic, data);
 
     if (topic === "irrigation/soil") soil.textContent = data;
     if (topic === "irrigation/voltage") volt.textContent = data;
@@ -46,7 +47,7 @@ window.addEventListener("load", () => {
     if (topic === "irrigation/threshold") threshold.value = data;
   });
 
-  // ===== CONTROL FUNCTIONS =====
+  // ===== CONTROL =====
   window.toggleMode = () => {
     client.publish("irrigation/cmd/mode", "TOGGLE");
   };
